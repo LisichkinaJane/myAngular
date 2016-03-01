af52f193991cb0bc3abe52817f01375005dda7f3
Greg <p.greg1994@gmail.com>
2016-03-01 20:51:52 +0200

    010316

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('260216', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'toastr']);
+    .module('myAngular', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'toastr']);
 
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular
-    .module('260216')
+    .module('myAngular')
     .controller('MainController', MainController);
 
   /** @ngInject */
@@ -10,7 +10,46 @@
     var vm = this;
     vm.test ='hello World';
     vm.header ='todo List';
+    vm.btnText = "ADD:";
+    vm.items = [
+    {
+      name:"first",
+      completed: false,
+      id:0
+    },
+    {
+      name:"cecond",
+      completed: false,
+      id:1
+    }
+    ];
     vm.newTodo =null;
+
+    vm.addItem = function(e){
+      var model = null;
+     
+      if(e.keyCode ===13 || e.type === 'click'){
+        model = {
+          name:vm.newTodo,
+          completed: false,
+          id:generateId 
+      };
+      vm.items.push(model);
+      vm.newTodo =null;
+      }
+    },
+    vm.deleteItem = function(e, id){
+       var currentIndex = this.items.indexOf(this.items.filter(function (item) {
+            return item.id === parseInt(id);
+        })[0]);
+
+      vm.items.splice(currentIndex, 1);
+    
+    },
+
+  function generateId () {
+        return Math.floor((1 + Math.random()) * 0x10000);
+    }
 
   }
 })();
